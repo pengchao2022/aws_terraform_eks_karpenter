8bbb4f5c27f19d783e6da8c33552bd92f881e9b7
pengchao2022 <pengchao.ma2@outlook.com>
2026-06-05 02:54:29 +0800

    feat: GTP-54273 modified the code

--- provider.tf
+++ provider.tf
@@ -1,13 +1,16 @@
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.5.0"  # this is for lowest version of terraform to run this project 
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.80" 
+      source  = "hashicorp/aws" # this is to tell terraform to go to hashicorp terraform registry then download aws plugin
+      version = "~> 5.80"       # the aws plugin version in hashicorp should greater than 5.80
     }
   }
 }
 
 provider "aws" {
-  region = var.aws_region
+  region = var.aws_region # here declare the target region 
+                          # here requires aws credentials
+                          # the credential can be aws configure or for github secrets OIDC AWS_IAM_Role
+                          # OIDC is for OpenID Connect
 }
